7c0c2861cc291c0c7a6b48350e84f1cd0d3d96c5
felixinc <fhoff@web.de>
2014-04-24 01:23:18 +0200

    This wraps the remote.sh in a simple web server

---
+++ server.sh
@@ -0,0 +1,13 @@
+echo Trying to create pipe
+mkfifo /tmp/backpipe
+echo
+
+
+while true
+do
+  echo Listening
+  ./remote.sh < /tmp/backpipe | nc -l 8080 > /tmp/backpipe
+  echo Done
+  echo
+done
+
